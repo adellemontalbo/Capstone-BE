62e18378ab90ded7785a2076483caf2ec7ec0c2f
Adelle Montalbo <adellemontalbo@gmail.com>
2023-01-25 14:07:57 -0800

    set up url patterns and views for products

---
+++ base/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from . import views
+
+#list of urls (paths)
+#we need to tell our django project (in backend), whenever we enter the homepage, send the traffic to our first path (left blank because it's the home url)
+# whenever we type in said route we should trigger the appropriate view
+urlpatterns = [
+    path('', views.getRoutes, name="routes"),
+    path('products/', views.getProducts, name="products"),
+]
--- base/views.py
+++ base/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .products import products
 
 # Create your views here.
+
+# first view will tell us what routes we have and how our API is going to look
+def getRoutes(request):
+    #routes for our products
+    routes = [
+        '/api/products/',
+        '/api/products/create',
+        '/api/products/upload',
+        '/api/products/<id>',
+        '/api/products/<delete>/<id>/',
+         '/api/products/<update>/<id>'
+    ]
+    return JsonResponse(routes, safe=False)
+
+#now we need to configure our urls to know when to trigger this view, becuase we're going to have multiple views. We connect our view to urls in urls.py file
+
+
+# creating another view, later on we'll actually query the db and get back real products
+def getProducts(request):
+    return JsonResponse(products, safe=False)
