2dd7a0347d9c741cf5960e2da689456f09e964df
Adelle Montalbo <adellemontalbo@gmail.com>
2023-01-25 14:31:15 -0800

    now using api_view and Response

--- base/urls.py
+++ base/urls.py
@@ -7,4 +7,5 @@ from . import views
 urlpatterns = [
     path('', views.getRoutes, name="routes"),
     path('products/', views.getProducts, name="products"),
+    path('products/<id>', views.getProduct, name="product"),
 ]
--- base/views.py
+++ base/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .products import products
 
 # Create your views here.
 
 # first view will tell us what routes we have and how our API is going to look
+@api_view(['GET']) # put methods we allow here
 def getRoutes(request):
     #routes for our products
     routes = [
@@ -15,11 +18,23 @@ def getRoutes(request):
         '/api/products/<delete>/<id>/',
          '/api/products/<update>/<id>'
     ]
-    return JsonResponse(routes, safe=False)
+    return Response(routes)
 
 #now we need to configure our urls to know when to trigger this view, becuase we're going to have multiple views. We connect our view to urls in urls.py file
 
 
 # creating another view, later on we'll actually query the db and get back real products
+# Get all products
+@api_view(['GET'])
 def getProducts(request):
-    return JsonResponse(products, safe=False)
+    return Response(products)
+
+@api_view(['GET'])
+def getProduct(request, id): #pk for primary key
+    product = None
+    for item in products:
+        if item['id'] == id:
+            product = item
+            break
+    return Response(product)
+
