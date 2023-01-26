52506e6a4dd1d34133234ff028e686e8748b84d5
Adelle Montalbo <adellemontalbo@gmail.com>
2023-01-26 10:21:09 -0800

    set up db environ variables

--- base/views.py
+++ base/views.py
@@ -4,8 +4,6 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .products import products
 
-# Create your views here.
-
 # first view will tell us what routes we have and how our API is going to look
 @api_view(['GET']) # put methods we allow here
 def getRoutes(request):
@@ -20,17 +18,15 @@ def getRoutes(request):
     ]
     return Response(routes)
 
-#now we need to configure our urls to know when to trigger this view, becuase we're going to have multiple views. We connect our view to urls in urls.py file
-
-
-# creating another view, later on we'll actually query the db and get back real products
+# later on we'll actually query the db and get back real products
 # Get all products
 @api_view(['GET'])
 def getProducts(request):
     return Response(products)
 
+#get one product
 @api_view(['GET'])
-def getProduct(request, id): #pk for primary key
+def getProduct(request, id): 
     product = None
     for item in products:
         if item['id'] == id:
