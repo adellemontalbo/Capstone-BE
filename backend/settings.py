6f3db62c1147d925156181a66d594e0611528be3
Adelle Montalbo <adellemontalbo@gmail.com>
2023-01-30 10:42:19 -0800

    installed django cors headers

--- backend/settings.py
+++ backend/settings.py
@@ -44,11 +44,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'base.apps.BaseConfig',
+    
     'rest_framework',
+    'corsheaders',
+    'base.apps.BaseConfig',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -138,3 +142,5 @@ MEDIA_ROOT = 'static/images'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
