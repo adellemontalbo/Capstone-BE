80fdabb9bc130c3b59bf2326791a0720668b5c8d
Adelle Montalbo <adellemontalbo@gmail.com>
2023-02-05 15:58:45 -0800

    changes to order model I want to make,not yet made

--- base/models.py
+++ base/models.py
@@ -34,7 +34,9 @@ class Order(models.Model):
     # createdAt = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return str(self.createdAt)
+        # I want to change it to this
+        return str(self.id)
+        # return str(self.createdAt)
     
 class OrderItem(models.Model):
     id = models.AutoField(primary_key=True, editable=False)
