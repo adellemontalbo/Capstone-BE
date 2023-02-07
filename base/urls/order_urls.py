740da0cd842d95dbfc0e000f35badc837b681c1c
Jessica H <jessicahebert75@gmail.com>
2023-02-07 10:45:55 -0800

    order profilescreen added

--- base/urls/order_urls.py
+++ base/urls/order_urls.py
@@ -5,6 +5,7 @@ from base.views import order_views as views
 urlpatterns = [
 
 path('add/', views.addOrderItems, name='orders-add'), 
+path('myorders/', views.getMyOrders, name='myorders'), 
 path('<str:id>/', views.getOrderById, name='user-order'), 
 path('<str:id>/pay', views.updateOrderToPaid, name='paid'), 
 ]
--- base/views/order_views.py
+++ base/views/order_views.py
@@ -85,4 +85,11 @@ def updateOrderToPaid(request, id):
     order.isPaid = True
     order.save()
     return Response('Order paid for')
-   
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getMyOrders(request): 
+    user= request.user
+    orders = user.order_set.all()
+    serializer = OrderSerializer(orders , many = True)
+    return Response(serializer.data)
