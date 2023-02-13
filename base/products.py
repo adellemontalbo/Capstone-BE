d7e641d6ab92a8ca483a6a90f12730cff386c42c
Adelle Montalbo <adellemontalbo@gmail.com>
2023-02-13 09:23:51 -0800

    changed prices

--- base/products.py
+++ base/products.py
@@ -5,7 +5,7 @@ products = [
         'image': 'https://images.pexels.com/photos/1413420/pexels-photo-1413420.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
         'description': 'Handcrafted blue earrings',
         'category': 'Jewelry',
-        'price': 15.99,
+        'price': 18.00,
         'countInStock' : 100
     },
     {       
@@ -14,16 +14,16 @@ products = [
         'image': 'https://images.pexels.com/photos/13595744/pexels-photo-13595744.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
         'description': 'Handcrafted natural round shaped earrings',
         'category': 'Jewelry',
-        'price': 15.99,
+        'price':16.00,
         'countInStock' : 100
     },
     {       
         'id': '3',
-        'name': 'Painting 1',
+        'name': 'Splash Painting',
         'image': 'https://images.pexels.com/photos/12868017/pexels-photo-12868017.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
         'description': 'Handcrafted painting',
         'category': 'Paintings',
-        'price': 100.00,
+        'price': 40.00,
         'countInStock' : 100
     },
        {       
